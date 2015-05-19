ab2cc95e927000c02e730a4bbf70373f1bb6dd48
Julio Cesar Silva Guzman <julio.sguz@gmail.com>
2015-05-19 02:56:56 -0400

    New features

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
 var gulp = require('gulp'),
-	gulpPlugins = require('gulp-load-plugins')(),
-	runSequence = require('run-sequence'),
-	browserSync = require('browser-sync'),
-  del = require('del'),
-	config = require('./config.json'),
-	reload = browserSync.reload,
-	$ = gulpPlugins,
-	AUTOPREFIXER_BROWSERS = [
-		'ie >= 10',
-		'ie_mob >= 10',
-		'ff >= 30',
-		'chrome >= 34',
-		'safari >= 7',
-		'opera >= 23',
-		'ios >= 7',
-		'android >= 4.4',
-		'bb >= 10'
-	];
+    gulpPlugins = require('gulp-load-plugins')(),
+    runSequence = require('run-sequence'),
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    config = require('./config.json'),
+    reload = browserSync.reload,
+    $ = gulpPlugins,
+    AUTOPREFIXER_BROWSERS = [
+      'ie >= 10',
+      'ie_mob >= 10',
+      'ff >= 30',
+      'chrome >= 34',
+      'safari >= 7',
+      'opera >= 23',
+      'ios >= 7',
+      'android >= 4.4',
+      'bb >= 10'
+    ];
 
 gulp.task('jshint', function () {
-  return gulp.src( config.app.jsDir + '/*.js' )
+  return gulp.src( config.app.js.sources )
     .pipe(reload({stream: true, once: true}))
     .pipe($.jshint())
     .pipe($.jshint.reporter(config.app.js.jsHintReporter))
@@ -31,15 +31,29 @@ gulp.task('styles', function () {
     .pipe($.sourcemaps.init())
     .pipe($.changed(config.app.css.tmpDirectory, {extension: '.css'}))
     .pipe($.stylus())
-    .pipe($.autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
-    .pipe($.sourcemaps.write())
+    .pipe($.autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))    
+    .pipe($.sourcemaps.write())    
     .pipe(gulp.dest(config.app.css.tmpDirectory))
-    /*.pipe($.if('*.css', $.csso()))
-    .pipe(gulp.dest('deploy/styles'))*/
     .pipe($.size({title: 'Styles'}));
 });
 
-gulp.task('server', ['styles'], function () {
+gulp.task('concat-js', function(){
+  return gulp.src(config.files.concat.js)            
+    .pipe($.changed(config.app.js.tmpDirectory, {extension: '.js'}))
+    .pipe($.if('*.js', $.concat( config.app.js.concat )))  
+    .pipe($.if('*.js',gulp.dest(config.app.js.tmpDirectory)))   
+    .pipe($.size({title: 'Concat JS size'}));
+});
+
+gulp.task('concat-css', function(){
+  return gulp.src(config.files.concat.css)            
+    .pipe($.changed(config.app.css.tmpDirectory, {extension: '.css'}))    
+    .pipe($.if('*.css', $.concat( config.app.css.concat )))    
+    .pipe($.if('*.css', gulp.dest(config.app.css.tmpDirectory)))
+    .pipe($.size({title: 'Concat CSS size'}));
+});
+
+gulp.task('server', ['styles','concat-js','concat-css'], function () {
   browserSync({
     notify: false,    
     logPrefix: 'Initial Layout',    
@@ -47,26 +61,24 @@ gulp.task('server', ['styles'], function () {
   });
 
   gulp.watch(config.files.watch.html, reload);
-  gulp.watch(config.files.watch.styles, ['styles', reload]); // edited by Julio 
-  gulp.watch(config.files.watch.js, ['jshint']);
+  gulp.watch(config.files.watch.styles, ['styles', reload]);
+  gulp.watch(config.files.watch.js, ['jshint',reload]);
   gulp.watch(config.files.watch.img, reload);
 });
 
 gulp.task('clean', del.bind(null, config.files.cleanPaths, {dot: true}));
 
-//var assets = $.useref.assets({searchPath: '{.tmp,app}'});
-
-gulp.task('deploy', function () {
+gulp.task('server:deploy', ['clean'],function () {
   return gulp.src(config.deploy.from, {
     dot: true
   }).pipe(gulp.dest(config.deploy.to))
-    .pipe($.size({title: 'copy'}));
+    .pipe($.size({title: 'copy'}))
+    .pipe($.if('**/*.css', $.csso()))
+    .pipe($.if('**/*.js', $.uglify({preserveComments: 'some'})))
+    .pipe(gulp.dest(config.deploy.to));
 });
 
 // Build production files, the default task
 gulp.task('default', ['clean'], function (cb) {
   runSequence('styles', ['jshint','deploy'], cb);
 });
-
-// Load custom tasks from the `tasks` directory
-//try { require('require-dir')('tasks'); } catch (err) { console.error(err); }
